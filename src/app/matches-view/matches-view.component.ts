e43ea2d0cde0758530db515043cfb5428fe68b97
405477TomasColazo <405477@tecnicatura.frc.utn.edu.ar>
2024-08-30 15:14:16 -0300

    if y for

--- src/app/matches-view/matches-view.component.ts
+++ src/app/matches-view/matches-view.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { MatchInfoComponent } from '../match-info/match-info.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-matches-view',
   standalone: true,
-  imports: [MatchInfoComponent],
+  imports: [MatchInfoComponent, NgIf, NgFor],
   templateUrl: './matches-view.component.html',
-  styleUrl: './matches-view.component.css'
+  styleUrl: './matches-view.component.css',
 })
 export class MatchesViewComponent {
-  matches: string[] = [
-    "Equipo1 vs Equipo2",
-    "Equipo3 vs Equipo4"
-  ];
-  matchSelected: string = "";
+  matches: string[] = ['Equipo1 vs Equipo2', 'Equipo3 vs Equipo4'];
+  matchSelected: string = '';
 
   onSelected(selected: string) {
     this.matchSelected = selected;
