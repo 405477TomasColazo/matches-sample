79f9fa6f7ae656ed30d036e2f375c086fb6763bc
405477TomasColazo <405477@tecnicatura.frc.utn.edu.ar>
2024-08-30 16:51:03 -0300

    fin de la clase

--- src/app/match-info/match-info.component.ts
+++ src/app/match-info/match-info.component.ts
@@ -1,14 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-match-info',
   standalone: true,
   imports: [],
   templateUrl: './match-info.component.html',
-  styleUrl: './match-info.component.css'
+  styleUrl: './match-info.component.css',
 })
-export class MatchInfoComponent {
-  @Input() match: string = "";
+export class MatchInfoComponent implements OnDestroy {
+  ngOnDestroy(): void {
+    alert('Destruido');
+  }
+  @Input() match: string = '';
   @Output() selectedMatch = new EventEmitter<string>();
 
   mostrarPartido() {
--- src/app/matches-view/matches-view.component.ts
+++ src/app/matches-view/matches-view.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatchInfoComponent } from '../match-info/match-info.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Match } from '../models/match';
 
 @Component({
   selector: 'app-matches-view',
@@ -10,11 +11,26 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './matches-view.component.html',
   styleUrl: './matches-view.component.css',
 })
-export class MatchesViewComponent {
-  matches: string[] = ['Equipo1 vs Equipo2', 'Equipo3 vs Equipo4'];
-  matchSelected: string = '';
+export class MatchesViewComponent implements OnInit {
+  ngOnInit(): void {
+    this.matches = [
+      {
+        name: 'Equipo1 vs Equipo2',
+      },
+      {
+        name: 'Equipo3 vs Equipo4',
+      },
+    ];
+  }
 
+  matches: Match[] = [];
+  matchSelected: string = '';
+  myDate: Date = new Date();
   onSelected(selected: string) {
     this.matchSelected = selected;
+  }
+
+  onDeleteLast() {
+    this.matches.pop();
   }
 }
